1efa18e811dab1fce106c648b2e949e18f74a788
JuliaKatze <32577940+JuliaKatze@users.noreply.github.com>
2017-10-09 18:24:46 +0300

    Update Lab4.cpp

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-const int i = 200; // òî÷êà 1
+const int i = 200; 
 int main()
 {
-char s[i]; //ìàññèâ
+char s[i]; 
 std::cout « "Please enter your text \n";
 std::cin.getline(s,200);
 
-for (int j = 0; j < i && (s[j] != '.'); j++) if (s[j] >= '0' && s[j] <= '9') std::cout « s[j]; exit; 
+for (int j = 0; j < i; j++) if (s[j] >= '0' && s[j] <= '9') std::cout « s[j]; exit; 
 std::cout « endl; 
 
-for (int j = 0; j < i && (s[j] != '.'); j++) if (s[j] < '0' || s[j] > '9') std::cout « s[j] ; exit; 
+for (int j = 0; j < i; j++) if (s[j] < '0' || s[j] > '9') std::cout « s[j] ; exit; 
 std::cout « endl;
 
 
