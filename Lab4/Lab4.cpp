b6bee1628d996e3cccf231289f1c69234b93fcd7
JuliaKatze <32577940+JuliaKatze@users.noreply.github.com>
2017-10-09 18:21:54 +0300

    Update Lab4.cpp

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,11 +1,20 @@
-// Lab4.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
-
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+const int i = 200; // òî÷êà 1
 int main()
 {
-    return 0;
-}
+char s[i]; //ìàññèâ
+std::cout « "Please enter your text \n";
+std::cin.getline(s,200);
 
+for (int j = 0; j < i && (s[j] != '.'); j++) if (s[j] >= '0' && s[j] <= '9') std::cout « s[j]; exit; 
+std::cout « endl; 
+
+for (int j = 0; j < i && (s[j] != '.'); j++) if (s[j] < '0' || s[j] > '9') std::cout « s[j] ; exit; 
+std::cout « endl;
+
+
+return 0;
+}
